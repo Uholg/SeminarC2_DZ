fcf837659d787423cb2e1a8d1b8fd5174aa8e16b
Olga Ukhanova <uhole4ka@rambler.ru>
2022-11-16 16:11:17 +0300

    Добавили решение Задач 10,13,15

---
+++ Z10/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+//и на выходе показывает вторую цифру этого числа.
+int randomNumber= new Random().Next(100,1000);
+Console.Write(randomNumber + " -> ");
+string a = Convert.ToString(randomNumber);
+
+Console.Write(a[1]);
---
+++ Z13/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+//или сообщает, что третьей цифры нет.
+int randomNumber= new Random().Next(1,100000);
+Console.Write(randomNumber + " -> ");
+string a = Convert.ToString(randomNumber);
+ if (a.Length < 3)
+{Console.Write("Третьей цифры нет");}
+else
+{Console.Write(a[2]);}
